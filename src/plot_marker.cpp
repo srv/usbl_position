80cfa56c9cdb166cfd2b814813ac814c8772dbd0
Eric Guerrero <ericguerrerofont@gmail.com>
2016-09-14 18:31:20 +0200

    compare filter with and without usbl

--- src/plot_marker.cpp
+++ src/plot_marker.cpp
@@ -1,158 +1,136 @@
-
 #include "ros/ros.h"
 #include "geometry_msgs/PoseWithCovarianceStamped.h"
 #include "geometry_msgs/PoseStamped.h"
 #include "nav_msgs/Path.h"
+#include "nav_msgs/Odometry.h"
 #include "visualization_msgs/Marker.h"
+#include "visualization_msgs/MarkerArray.h"
 
 #include <tf/transform_listener.h>
 
 using namespace std;
 
 
-class PlotPath
+class PlotMarker
 {
 public:
-  PlotPath()
+  PlotMarker(): nhp_("~")
   {
+    // Get params
+    nhp_.param("r", red_, 0.0);
+    nhp_.param("g", green_, 0.0);
+    nhp_.param("b", blue_, 0.0);
+    nhp_.param("a", alpha_, 1.0);
+    nhp_.param("size", size_, 0.5);
+
     // Node name
     node_name_ = ros::this_node::getName();
+    marker_id_ = 1;
 
-
-    //Topic you want to publish
-    delayed_pub_ = nhp_.advertise<nav_msgs::Path>("sensors/modem_delayed_path", 10);
-    raw_pub_ = nhp_.advertise<nav_msgs::Path>("sensors/modem_raw_path", 10);
-    // cov_pub = n.advertise<visualization_msgs::Marker>(node_name_+"/path" + "/cov", 10);
-
-    //Topic you want to subscribe
-    delayed_sub_ = nh_.subscribe("sensors/modem_delayed", 10, &PlotPath::delayedCallback, this);
-    raw_sub_ = nh_.subscribe("sensors/modem_raw", 10, &PlotPath::rawCallback, this);
+    // Topics
+    pub_ = nhp_.advertise<visualization_msgs::MarkerArray>("/topic_out", 10);
+    sub_ = nh_.subscribe("/topic_in", 10, &PlotMarker::callback, this);
   }
 
-  void delayedCallback(const geometry_msgs::PoseWithCovarianceStamped& position)
+
+  void callback(const geometry_msgs::PoseWithCovarianceStamped& msg)
+  {
+    //Transform position from modem_origin reference frame to map
+    geometry_msgs::PoseStamped trans;
+    changeFrame(msg,trans);
+
+    //Create marker and publish
+    visualization_msgs::Marker marker;
+    createMarker(trans, marker);
+    
+    array_.markers.push_back(marker);
+    pub_.publish(array_);
+  }
+
+
+  void changeFrame(const geometry_msgs::PoseWithCovarianceStamped& position,
+                  geometry_msgs::PoseStamped& trans)
   {
     // TF map-modem_origin
     tf::StampedTransform  map2origin;
+    getModemOriginTF(map2origin);
+
+    // TF modem_origin-sparus
+    tf::Transform origin2sparus;
+    getSparusTF(position, origin2sparus);
+
+    //TF map-sparus
+    tf::Transform map2sparus = map2origin * origin2sparus;
+
+    //Build message
+    trans.header.stamp = position.header.stamp;
+    trans.header.frame_id = "map";
+    trans.pose.position.x = map2sparus.getOrigin().x();
+    trans.pose.position.y = map2sparus.getOrigin().y();
+    trans.pose.position.z = map2sparus.getOrigin().z();
+  }
+
+  void getModemOriginTF(tf::StampedTransform& map2origin)
+  {
     try
     {
       listener_.waitForTransform("/map", "modem_origin", ros::Time(0), ros::Duration(2));
       listener_.lookupTransform("/map", "modem_origin", ros::Time(0), map2origin);
     }
     catch (tf::TransformException ex){
-      ROS_ERROR_STREAM("Received an exception trying to transform a USBL point: " << ex.what());
+      ROS_ERROR_STREAM("[" << node_name_ << "]: Received an exception trying to transform a USBL point: " << ex.what());
     }
-
-    //TF modem_origin-sparus
-    tf::Transform  origin2sparus;
-    tf::Vector3 origin2sparus_v(position.pose.pose.position.x, position.pose.pose.position.y, position.pose.pose.position.z);
-    tf::Quaternion origin2sparus_q(0.0, 0.0, 0.0, 1);
-    origin2sparus.setOrigin(origin2sparus_v);
-    origin2sparus.setRotation(origin2sparus_q);
-
-    //TF map-sparus
-    tf::Transform  map2sparus = map2origin * origin2sparus;
-
-    geometry_msgs::PoseStamped p;
-    p.pose.position.x = map2sparus.getOrigin().x();
-    p.pose.position.y = map2sparus.getOrigin().y();
-    p.pose.position.z = map2sparus.getOrigin().z();
-
-    delayed_path_.header.stamp = position.header.stamp;
-    delayed_path_.header.frame_id = "map";
-    delayed_path_.poses.push_back(p);
-    delayed_pub_.publish(delayed_path_);
-
-    // visualization_msgs::Marker marker;
-    // marker.header.stamp = position.header.stamp;
-    // marker.header.frame_id = "map";
-    // marker.ns = node_name_+"/path" + "_pose_cov";
-    // marker.id = id_;
-    // marker.type = 2;  // SPHERE
-    // marker.action = 0; // ADD
-    // marker.pose.position.x = position.pose.pose.position.x;
-    // marker.pose.position.y = position.pose.pose.position.y;
-    // marker.pose.position.z = position.pose.pose.position.z;
-    // marker.scale.x = position.pose.covariance[0];
-    // marker.scale.y = position.pose.covariance[7];
-    // marker.scale.z = position.pose.covariance[14];
-    // marker.color.r = 1.0;
-    // marker.color.g = 0.0;
-    // marker.color.b = 0.0;
-    // marker.color.a = 0.5;
-    // cov_pub_.publish(marker);
   }
 
-void rawCallback(const geometry_msgs::PoseWithCovarianceStamped& position)
+  void getSparusTF(const geometry_msgs::PoseWithCovarianceStamped& position,
+                   tf::Transform& origin2sparus)
   {
-    // TF map-modem_origin
-    tf::StampedTransform  map2origin;
-    try
-    {
-      listener_.waitForTransform("/map", "modem_origin", ros::Time(0), ros::Duration(2));
-      listener_.lookupTransform("/map", "modem_origin", ros::Time(0), map2origin);
-    }
-    catch (tf::TransformException ex){
-      ROS_ERROR_STREAM("Received an exception trying to transform a USBL point: " << ex.what());
-    }
-
-    //TF modem_origin-sparus
-    tf::Transform  origin2sparus;
     tf::Vector3 origin2sparus_v(position.pose.pose.position.x, position.pose.pose.position.y, position.pose.pose.position.z);
     tf::Quaternion origin2sparus_q(0.0, 0.0, 0.0, 1);
     origin2sparus.setOrigin(origin2sparus_v);
     origin2sparus.setRotation(origin2sparus_q);
-
-    //TF map-sparus
-    tf::Transform  map2sparus = map2origin * origin2sparus;
-
-    geometry_msgs::PoseStamped p;
-    p.pose.position.x = map2sparus.getOrigin().x();
-    p.pose.position.y = map2sparus.getOrigin().y();
-    p.pose.position.z = map2sparus.getOrigin().z();
-
-    raw_path_.header.stamp = position.header.stamp;
-    raw_path_.header.frame_id = "map";
-    raw_path_.poses.push_back(p);
-    raw_pub_.publish(raw_path_);
-
-    // visualization_msgs::Marker marker;
-    // marker.header.stamp = position.header.stamp;
-    // marker.header.frame_id = "map";
-    // marker.ns = node_name_+"/path" + "_pose_cov";
-    // marker.id = id_;
-    // marker.type = 2;  // SPHERE
-    // marker.action = 0; // ADD
-    // marker.pose.position.x = position.pose.pose.position.x;
-    // marker.pose.position.y = position.pose.pose.position.y;
-    // marker.pose.position.z = position.pose.pose.position.z;
-    // marker.scale.x = position.pose.covariance[0];
-    // marker.scale.y = position.pose.covariance[7];
-    // marker.scale.z = position.pose.covariance[14];
-    // marker.color.r = 1.0;
-    // marker.color.g = 0.0;
-    // marker.color.b = 0.0;
-    // marker.color.a = 0.5;
-    // cov_pub_.publish(marker);
   }
 
+  void createMarker(const geometry_msgs::PoseStamped& trans,
+                          visualization_msgs::Marker& marker)
+  {
+    marker.header.frame_id = "map";
+    marker.pose = trans.pose;
+    marker.type = 2;  // SPHERE
+    marker.color.r = red_;
+    marker.color.g = green_;
+    marker.color.b = blue_;
+    marker.color.a = alpha_;
+    marker.scale.x = size_;
+    marker.scale.y = size_;
+    marker.scale.z = size_;
 
+    marker.id = marker_id_;
+    marker_id_++;
+
+    marker.action = 0; // ADD
+    marker.lifetime = ros::Duration(600);
+  }
 
 private:
   ros::NodeHandle nh_; 
   ros::NodeHandle nhp_;
-  ros::Publisher delayed_pub_;
-  ros::Publisher raw_pub_;
-  ros::Publisher cov_pub_;
-  ros::Subscriber delayed_sub_;
-  ros::Subscriber raw_sub_;
+
+  ros::Publisher pub_;
+  ros::Subscriber sub_;
 
   tf::TransformListener listener_;
 
-  nav_msgs::Path delayed_path_;
-  nav_msgs::Path raw_path_;
-  int id_;
+  visualization_msgs::MarkerArray array_;
 
   string node_name_;
+  double red_;
+  double green_;
+  double blue_;
+  double alpha_;
+  double size_;
+
+  int marker_id_;
 
 };//End of class PlotPath
 
@@ -160,11 +138,10 @@ private:
 int main(int argc, char **argv)
 {
   //Initiate ROS
-  ros::init(argc, argv, "plot_path");
+  ros::init(argc, argv, "plot_marker");
 
-  PlotPath SAPObject;
+  PlotMarker Marker;
 
   ros::spin();
   return 0;
 }
-
--- src/plot_path.cpp
+++ src/plot_path.cpp
@@ -1,4 +1,3 @@
-
 #include "ros/ros.h"
 #include "geometry_msgs/PoseWithCovarianceStamped.h"
 #include "geometry_msgs/PoseStamped.h"
@@ -19,183 +18,40 @@ public:
   {
     // Node name
     node_name_ = ros::this_node::getName();
-    marker_id_delayed_ = 1;
-    marker_id_raw_ = 1;
 
-    //Topic you want to publish
-    pub_raw_ = nhp_.advertise<visualization_msgs::MarkerArray>("plot/modem_raw", 10);
-    pub_delayed_ = nhp_.advertise<visualization_msgs::MarkerArray>("plot/modem_delayed", 10);
-    pub_ekfodom_ = nhp_.advertise<nav_msgs::Path>("plot/ekf_odom", 10);
-    pub_ekfmap_ = nhp_.advertise<nav_msgs::Path>("plot/ekf_map", 10);
-
-
-    //Topic you want to subscribe
-    sub_delayed_ = nh_.subscribe("/sensors/modem_delayed_acoustic", 10, &PlotPath::delayedCallback, this);
-    sub_raw_ = nh_.subscribe("/sensors/modem_raw", 10, &PlotPath::rawCallback, this);
-    sub_ekfodom_ = nh_.subscribe("/ekf_odom/odometry", 10, &PlotPath::odomCallback, this);
-    sub_ekfmap_ = nh_.subscribe("/ekf_map/odometry", 10, &PlotPath::mapCallback, this);
+    // Topics
+    pub_ = nhp_.advertise<nav_msgs::Path>("topic_out", 10);
+    sub_ = nh_.subscribe("topic_in", 10, &PlotPath::mapCallback, this);
   }
 
-  void odomCallback(const nav_msgs::Odometry& ekfodom)
+
+  void mapCallback(const nav_msgs::Odometry& msg)
   {
-    geometry_msgs::PoseStamped pose;
-    pose.pose = ekfodom.pose.pose;
-    pose.header.stamp = ekfodom.header.stamp;
-    pose.header.frame_id = "map";
+    geometry_msgs::PoseStamped position;
+    position.pose = msg.pose.pose;
+    position.header.stamp = msg.header.stamp;
+    position.header.frame_id = "map";
 
-    path_ekfodom_.header.stamp = ekfodom.header.stamp;
-    path_ekfodom_.header.frame_id = "map";
-    path_ekfodom_.poses.push_back(pose);
-    pub_ekfodom_.publish(path_ekfodom_);
+    path_.header.stamp = msg.header.stamp;
+    path_.header.frame_id = "map";
+    path_.poses.push_back(position);
+    pub_.publish(path_);
   }
 
-  void mapCallback(const nav_msgs::Odometry& ekfmap)
-  {
-    geometry_msgs::PoseStamped pose;
-    pose.pose = ekfmap.pose.pose;
-    pose.header.stamp = ekfmap.header.stamp;
-    pose.header.frame_id = "map";
-
-    path_ekfmap_.header.stamp = ekfmap.header.stamp;
-    path_ekfmap_.header.frame_id = "map";
-    path_ekfmap_.poses.push_back(pose);
-    pub_ekfmap_.publish(path_ekfmap_);
-  }
-
-  void delayedCallback(const geometry_msgs::PoseWithCovarianceStamped& delayed)
-  {
-    //Transform position from modem_origin reference frame to map
-    geometry_msgs::PoseStamped trans_delayed;
-    changeFrame(delayed,trans_delayed);
-
-    //Create marker and publish
-    visualization_msgs::Marker marker_delayed;
-    createMarker(trans_delayed, marker_delayed, marker_id_delayed_, "blue", 0.2);
-    marker_id_delayed_++;
-    array_delayed_.markers.push_back(marker_delayed);
-    pub_delayed_.publish(array_delayed_);
-  }
-
-  void rawCallback(const geometry_msgs::PoseWithCovarianceStamped& raw)
-  {
-    //Transform position from modem_origin reference frame to map
-    geometry_msgs::PoseStamped trans_raw;
-    changeFrame(raw,trans_raw);
-
-    //Create marker and publish
-    visualization_msgs::Marker marker_raw;
-    createMarker(trans_raw, marker_raw, marker_id_raw_, "green", 0.5);
-    marker_id_raw_++;
-    array_raw_.markers.push_back(marker_raw);
-    pub_raw_.publish(array_raw_);
-  }
-
-  void changeFrame(const geometry_msgs::PoseWithCovarianceStamped& position,
-                  geometry_msgs::PoseStamped& transformed_position)
-  {
-    // TF map-modem_origin
-    tf::StampedTransform  map2origin;
-    getModemOriginTF(map2origin);
-
-    // TF modem_origin-sparus
-    tf::Transform origin2sparus;
-    getSparusTF(position, origin2sparus);
-
-    //TF map-sparus
-    tf::Transform map2sparus = map2origin * origin2sparus;
-
-    //Build message
-    transformed_position.header.stamp = position.header.stamp;
-    transformed_position.header.frame_id = "map";
-    transformed_position.pose.position.x = map2sparus.getOrigin().x();
-    transformed_position.pose.position.y = map2sparus.getOrigin().y();
-    transformed_position.pose.position.z = map2sparus.getOrigin().z();
-  }
-
-  void getModemOriginTF(tf::StampedTransform& map2origin)
-  {
-    try
-    {
-      listener_.waitForTransform("/map", "modem_origin", ros::Time(0), ros::Duration(2));
-      listener_.lookupTransform("/map", "modem_origin", ros::Time(0), map2origin);
-    }
-    catch (tf::TransformException ex){
-      ROS_ERROR_STREAM("[" << node_name_ << "]: Received an exception trying to transform a USBL point: " << ex.what());
-    }
-  }
-
-  void getSparusTF(const geometry_msgs::PoseWithCovarianceStamped& position,
-                   tf::Transform& origin2sparus)
-  {
-    tf::Vector3 origin2sparus_v(position.pose.pose.position.x, position.pose.pose.position.y, position.pose.pose.position.z);
-    tf::Quaternion origin2sparus_q(0.0, 0.0, 0.0, 1);
-    origin2sparus.setOrigin(origin2sparus_v);
-    origin2sparus.setRotation(origin2sparus_q);
-  }
-
-  void createMarker(const geometry_msgs::PoseStamped& transformed_position,
-                          visualization_msgs::Marker& marker,
-                          int& id, string color, float size)
-  {
-    // marker.header.stamp = ros::Time::now();
-    marker.header.frame_id = "map";
-    // marker.ns = "modem_delayed_fix";
-    marker.pose = transformed_position.pose;
-    marker.type = 2;  // SPHERE
-    marker.color.r = 0.0;
-    marker.color.g = 0.0;
-    marker.color.b = 0.0;
-    marker.color.a = 0.8;
-    if (color == "red")
-    {
-      marker.color.r = 1.0;
-    }
-    else 
-    {
-      if (color == "green")
-      {
-        marker.color.g = 1.0;
-      }
-      else
-      {
-        marker.color.b = 1.0;
-      }
-    }
-    marker.scale.x = size;
-    marker.scale.y = size;
-    marker.scale.z = size;
-
-    marker.id = id;
-
-    marker.action = 0; // ADD
-    marker.lifetime = ros::Duration(600);
-  }
 
 private:
   ros::NodeHandle nh_; 
   ros::NodeHandle nhp_;
 
-  ros::Publisher pub_raw_;
-  ros::Publisher pub_delayed_;
-  ros::Publisher pub_ekfodom_;
-  ros::Publisher pub_ekfmap_;
-
-  ros::Subscriber sub_delayed_;
-  ros::Subscriber sub_raw_;
-  ros::Subscriber sub_ekfodom_;
-  ros::Subscriber sub_ekfmap_;
+  ros::Publisher pub_;
+  ros::Subscriber sub_;
 
 
-  tf::TransformListener listener_;
-
-  nav_msgs::Path path_ekfodom_;
-  nav_msgs::Path path_ekfmap_;
-  visualization_msgs::MarkerArray array_delayed_;
-  visualization_msgs::MarkerArray array_raw_;
+  nav_msgs::Path path_;
 
   string node_name_;
-  int marker_id_delayed_;
-  int marker_id_raw_;
+  string topic_in_;
+  string topic_out_;
 
 };//End of class PlotPath
 
